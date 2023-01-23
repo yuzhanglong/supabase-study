3047168af66de9054adf9c7547fbfdb872565ad2
yuzhanglong <loveyzl1123@gmail.com>
2023-01-23 17:55:16 +0800

    chore: format code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { createClient } from '@supabase/supabase-js'
+import {createClient} from '@supabase/supabase-js'
 import {useEffect} from "react";
 
+const supabase = createClient(
+  'https://occxtuuwwbfoduajfopr.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jY3h0dXV3d2Jmb2R1YWpmb3ByIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQxMTg2NDYsImV4cCI6MTk4OTY5NDY0Nn0.ZQ99XqHXkrukpCX6tKJDrmP5zMl3h-ulDfqfMy-n1sI'
+)
+
+
 function App() {
-
-  const supabase = createClient('https://occxtuuwwbfoduajfopr.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jY3h0dXV3d2Jmb2R1YWpmb3ByIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQxMTg2NDYsImV4cCI6MTk4OTY5NDY0Nn0.ZQ99XqHXkrukpCX6tKJDrmP5zMl3h-ulDfqfMy-n1sI')
-
   async function getCountries() {
     const countries = await supabase.from('countries').select()
     console.log(countries)
@@ -18,7 +21,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
