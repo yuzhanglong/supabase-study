acea0eb87665c5dd18fefa088b2154a5039382b6
yuzhanglong <loveyzl1123@gmail.com>
2023-01-23 18:03:36 +0800

    feat: add api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {createClient} from '@supabase/supabase-js'
 import {useEffect} from "react";
@@ -11,7 +10,7 @@ const supabase = createClient(
 
 function App() {
   async function getCountries() {
-    const countries = await supabase.from('countries').select()
+    const countries = await supabase.from('countries').select('*');
     console.log(countries)
   }
 
@@ -20,20 +19,7 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     hello world!
     </div>
   );
 }
